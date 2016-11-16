3bd472034b13c5b2da9d6f2bc520fa732e8742aa
erikn <erik.nystrom87@gmail.com>
2016-11-15 20:41:10 -0500

    Heading

--- assign4.py
+++ assign4.py
@@ -1,1 +1,22 @@
 # Assignment 4 for CSC401 Python
+
+# FunctionOne(list of strings)
+# Take a list of strings as a parameter and return all unique strings
+def functionOne(stringList):
+
+# FunctionTwo(integer)
+# Take an integer and return whether or not the number is Perfect
+# Perfect number = sum of divisors = number
+
+# FunctionThree(integer)
+# Take an integer and return Perfect numbers <= number
+
+# FunctionFour(list of mixed types)
+# Take list of mixed type and count number of integers
+
+# FunctionFive(list of anything)
+# Take list of anything and remove second item
+
+# ClassCircle
+# Write class called circle, with an instance variable, for the radius
+# and methods to calculate the area and circumference of the circle.
